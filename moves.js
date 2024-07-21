993a5fc33c0f9628a0d3ca9db790c6da47f1bcdf
Nguyen Dao Quang Dang <nguyendang200004@gmail.com>
2024-07-22 00:11:59 +0700

    move

---
+++ moves.js
@@ -0,0 +1,35 @@
+var hinhanh = []
+var j = 0
+function loaded() {
+    for (let i = 0; i < 5; i++) {
+        hinhanh[i] = new Image();
+        hinhanh[i].src = `Ex/anh${i}.webp`
+    }
+}
+function found(prm) {
+    switch (prm) {
+        case 1:
+            j = 0;
+            break
+        case 2:
+            if (j > 0) {
+                j--;
+            }
+            break
+        case 3:
+            if (j < hinhanh.length - 1) {
+                j++
+            }
+            break
+        case 4:
+            j = hinhanh.length - 1
+            break
+    }
+    document.getElementById("imgage").src = hinhanh[j].src;
+    title();
+}
+
+function title() {
+    k = j + 1
+    document.getElementById("change").innerText = `${k}/${hinhanh.length}`
+}
